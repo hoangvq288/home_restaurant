566483124d6ff624a1e0c4d37ca6f11e634159ce
diepsohung <diepsohung@gmail.com>
2018-08-25 12:03:18 +0700

    Currency format for dishes & toppings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def date_time_format(date_time)
     date_time&.strftime('%d/%m/%Y at %I:%M%p')
   end
+
+  def currency_format(money)
+    "#{number_to_currency(money, precision: 0, unit: '', delimiter: ".")} VND"
+  end
 end
